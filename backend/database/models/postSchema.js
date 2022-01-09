bc83fa1b589165b2ec882a0d77b4bafe925b5af8
BasharAbuHannoud <abuhannoud.bashar@gmail.com>
2022-01-09 22:02:45 +0200

    add date on Post schema

--- backend/database/models/postSchema.js
+++ backend/database/models/postSchema.js
@@ -5,6 +5,7 @@ const postSchema = new mongoose.Schema({
   description: { type: String },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  date:{ type: Date, default: Date.now },
   role:{ type: mongoose.Schema.Types.ObjectId, ref: "Role" }
 });
 
