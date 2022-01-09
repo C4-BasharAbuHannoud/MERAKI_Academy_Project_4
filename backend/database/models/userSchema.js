f5d866e5c0f262f3e4ab6953531bddf79988831f
BasharAbuHannoud <abuhannoud.bashar@gmail.com>
2022-01-09 22:05:44 +0200

    add user schema

---
+++ backend/database/models/userSchema.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  userName: { type: String },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  age: { type: Number },
+  country: { type: String },
+  role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
+});
+
+module.exports = mongoose.model("Users", userSchema);
