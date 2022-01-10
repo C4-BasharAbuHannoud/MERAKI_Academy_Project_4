14bc91ee989f71fa4bb09a75f1828b101ab2d815
BasharAbuHannoud <abuhannoud.bashar@gmail.com>
2022-01-10 15:36:36 +0200

    push id comment on comment post

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -4,33 +4,41 @@ const commentsModel = require("../database/models/commentsSchema");
 //6-function to create new comment for user
 
 const createNewComment = (req, res) => {
-    const postId = req.params.post_id;
-    const { comment, commenter } = req.body;
-  
-    const newComment = new commentsModel({
-      comment,
-      commenter,
-    });
-  
-    newComment
+  const postId = req.params.post_id;
+  const { comment, commenter } = req.body;
+
+  const newComment = new commentsModel({
+    comment,
+    commenter,
+  });
+
+  newComment
     .save()
     .then((result) => {
-      res.status(201).json({
-        success: true,
-        message: " The new comment added",
-        result,
+      postsModel.updateOne(
+        { _id: postId },
+        { $push: { comments: result._id} }).then(() => {
+            res.status(201).json({
+              success: true,
+              message: `The new comment added`,
+              comment: result,
+            });
+          })
+          .catch((err) => {
+            res.status(500).json({
+              success: false,
+              message: `something went wrong while adding a new comment`,
+            });
+          })
+      })
+      .catch((err) => {
+        res.status(500).json({
+          success: false,
+          message: `Server Error`,
+        });
       });
-    })
-    .catch((err) => {
-      res.status(404).json({
-        success: false,
-        massage: "something went wrong while creating a new comment",
-      });
-    });
   };
-
-  
-  module.exports = {
-    createNewComment,
-  };
-  
+           
+module.exports = {
+  createNewComment,
+};
