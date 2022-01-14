f54566d2a899eea06cf58af999707077e1fe33d2
BasharAbuHannoud <abuhannoud.bashar@gmail.com>
2022-01-14 21:39:45 +0200

    Add props to navigation to show my profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
 
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Routes, Route, Link, useNavigate, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Login from "./comopnents/login";
 import Home from "./comopnents/Home";
 import NewPost from "./comopnents/newPost";
 import Profile from "./comopnents/profile";
+import Navigation from "./comopnents/Navigation";
 
 function App() {
   const [token, setToken] = useState("");
+  const [myId,setMyId]= useState("");
+ 
+  const navigate = useNavigate();
 
   useEffect(() => {
     const tokenStorage = localStorage.getItem("token");
+    console.log(tokenStorage);
     if (tokenStorage) {
       setToken(tokenStorage);
     }
@@ -19,53 +24,30 @@ function App() {
 
   return (
     <>
-      {token ? <div className="nav"> </div> : <> </>}
+      {token ? (
+    <Navigation setToken={setToken} myId={myId} />
+      ) : (
+        <> </>
+      )}
       <div className="App">
         <div className="Home">
           <Routes>
-            <Route path="/Home" element={<Home token={token} />} />
+            <Route
+              path="/Home"
+              element={<Home token={token} setMyId={setMyId} />}
+            />
             <Route path="/newPost" element={<NewPost token={token} />} />
             <Route path={`/profile/:id`} element={<Profile />} />
           </Routes>
         </div>
-        {token ? <Logout setToken={setToken} /> : <Login setToken={setToken} />}
+        {token ? <></> : <Login setToken={setToken} />}
       </div>
     </>
   );
 }
 
-const Navigation = () => {
-  return (
-    <div className="nav">
-      <div className="registerNav">
-        <Link to="/" className="linkreg">
-          Sign Up
-        </Link>
-      </div>
-      <div className="registerNav">
-        <Link to="/" className="linkreg">
-          Login
-        </Link>
-      </div>
-    </div>
-  );
-};
 
-const Logout = ({ setToken }) => {
-  const navigate = useNavigate();
 
-  return (
-    <button
-      className="LogOut"
-      onClick={(e) => {
-        localStorage.removeItem("token");
-        setToken("");
-        navigate("/");
-      }}
-    >
-      LogOut
-    </button>
-  );
-};
+
 
 export default App;
