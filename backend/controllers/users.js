430e21d02dde21e85c490c1d6736dff231934b34
BasharAbuHannoud <abuhannoud.bashar@gmail.com>
2022-01-16 13:39:25 +0200

    get info user by id

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -25,16 +25,42 @@ const createNewUser = (req, res) => {
       });
     })
     .catch((err) => {
-      if (err.code == 11000) {
-        res
-          .status(409)
-          .json({ success: false, massage: `The email or userName already exists` });
-      } else {
-        res.json(err);
+      if (err.keyPattern) {
+        return res.status(409).json({
+          success: false,
+          message: `The email already exists`,
+        });
       }
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err,
+      });
     });
 };
 
+const getInfoUser =(req,res)=>{
+  let id = req.params.info;
+  userModel
+    .findById(id)
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        massage: `The User ${id} `,
+        Info:[result],
+      });
+
+    
+    })
+    .catch((err) => {
+      res.status(404).json({
+        success: false,
+        massage: `The User ==> ${id} Not Found`,
+      });
+    });
+}
+
 module.exports = {
   createNewUser,
+  getInfoUser
 };
