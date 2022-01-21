602f0af76b050f76141aabde2ec60f7e6fef57f9
BasharAbuHannoud <abuhannoud.bashar@gmail.com>
2022-01-21 23:24:07 +0200

    add in navigation search, home , currunt account and logout

--- frontend/src/comopnents/Navigation.js
+++ frontend/src/comopnents/Navigation.js
@@ -1,49 +1,93 @@
-
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Routes, Route, Link, useNavigate, Navigate } from "react-router-dom";
-const Navigation = ({setToken,myId}) => {
- 
- 
+import { AiFillFacebook, AiFillHome } from "react-icons/ai";
+import { FaSistrix } from "react-icons/fa";
+//BsBoxArrowRight
+import { BsBoxArrowRight } from "react-icons/bs";
+
+const Navigation = ({ setToken, myId, allUsersSearch }) => {
+  const [search, setSearch] = useState("");
+
   const navigate = useNavigate();
 
- 
   return (
     <div className="nav">
-    <div className="logo_search">
-      <div className="logo">Logo</div>
-      <div className="search"></div>
-    </div>
+      <div className="logo_search">
+        <div className="logo">
+          <AiFillFacebook
+            className="facebook"
+            onClick={(e) => {
+              navigate(`/Home/${myId}`);
+            }}
+          />
+        </div>
+        <div className="search">
+          <FaSistrix
+            className="icont_seacrh"
+            onClick={(e) => {
+              allUsersSearch.find((element, i) => {
+                if (
+                  search.toLocaleLowerCase() ==
+                  element.userName.toLocaleLowerCase()
+                ) {
+                  navigate(`/profile/${element._id}`);
+                  setSearch("");
+                }
+              });
+            }}
+          />
+          <input
+            className="input_search"
+            type="text"
+            placeholder="Search Facebook"
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+        </div>
+      </div>
 
-    <div className="home_nav">
-      <Link to="/Home" className="linkreg">
-       Home
-        
-      </Link>
-    </div>
+      <div className="home_nav">
+        <Link to={`/Home/${myId}`}>
+          <AiFillHome className="home_icon" />
+        </Link>
+      </div>
 
-    <div>
-      {" "}
-      <Link className="linkreg" to={`/profile/${myId}`}>
-        My Profile
-      </Link>{" "}
+      <div>
+        {allUsersSearch &&
+          allUsersSearch.map((el, i) =>
+            el._id == myId ? (
+              <div className="nav_account">
+                <div >
+                  <img className="imge_account" src={el.image} />
+                </div>
+                <Link className="linkAccount" to={`/profile/${el._id}`}>
+                  <div className="userName_for_homePage">{el.userName}</div>
+                </Link>
+              </div>
+            ) : (
+              <></>
+            )
+          )}
+      </div>
+      <div className="style_logOut">
+        <BsBoxArrowRight className="icone_left" />
+        <button
+          className="LogOut"
+          onClick={(e) => {
+            console.log("hi");
+            localStorage.removeItem("token");
+            setToken("");
+            navigate("/");
+          }}
+        >
+          LogOut
+        </button>
+      </div>
     </div>
-    <div>
-    <button
-      className="LogOut"
-      onClick={(e) => {
-        console.log("hi");
-        localStorage.removeItem("token");
-        setToken("");
-        navigate("/");
-      }}
-    >
-      LogOut
-    </button>
-    </div>
-  </div>
   );
 };
-
-
 
 export default Navigation;
